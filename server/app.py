d33b58e534c12b979e0fa8c0b9b5271820df96ee
phamtruongvinh <truongvinh2112@gmail.com>
2022-01-16 23:08:37 +0700

    Update app.py
    add hello world

--- server/app.py
+++ server/app.py
@@ -11,6 +11,10 @@ def home():
     }
     return render_template('index.html', info=info)
 
+@app.route('/hello')
+def hello():
+    return 'hello world!'
+
 @app.route('/sum/<int:a>/<int:b>')
 def sum(a,b):
     return str(a+b)
